7b46d98495f5971dcb5183cc74a65ed2e1291af0
Pavlo Rodionov <v-prodionov@expedia.com>
2016-07-30 11:22:07 -0700

    adding sample controller

---
+++ tikify/src/main/java/com/epam/tm/web/Sample.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2015 Expedia, Inc. All rights reserved.
+ * EXPEDIA PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.epam.tm.web;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class Sample {
+
+    @RequestMapping("/ping")
+    @ResponseStatus(HttpStatus.OK)
+    public void test() {
+    }
+
+    @RequestMapping(value = "/echo", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody List<String> echo(@RequestBody String arg) {
+        return Arrays.asList(arg, arg, arg);
+    }
+
+    @RequestMapping(value = "/echo", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public @ResponseBody List<String> echo1(@RequestParam(required = true, value = "word") String arg) {
+        return Arrays.asList(arg, arg, arg);
+    }
+
+}
